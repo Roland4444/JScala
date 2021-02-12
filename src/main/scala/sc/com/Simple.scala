b2aac0a944a7b9835ff3a6d5aea845aa1d693244
Roland4444 <turbopollux@rambler.ru>
2021-02-12 04:58:52 +0400

    adding example hof

--- src/main/scala/sc/com/Simple.scala
+++ src/main/scala/sc/com/Simple.scala
@@ -2,5 +2,17 @@ package sc.com
 
 object Simple extends App{
   print("there works")
+  def result(foo: String, bar: String, Function:(String, String) => String): String={
+    val result = Function(foo, bar)
+    s"""<h1>$foo</h1><br>h1>$bar</h1><br>$result"""
+  }
+
+  def marinafunc(foo: String, bar: String): String="Σ="+String.valueOf(Integer.valueOf(foo)+Integer.valueOf(bar))
+  def guest(a: String, b: String): String=""
+
+  val foo="4"
+  val bar ="8"
+  println(result(foo,bar, marinafunc))
+  println(result(foo,bar, guest))
 
 }
